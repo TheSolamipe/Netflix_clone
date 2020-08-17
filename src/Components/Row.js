7372fc4c98d0c42d3c2d633f0a896ae89295aff5
TheSolamipe <akinolasolamipe@gmail.com>
2020-08-17 16:22:55 +0100

    Styled the row component

--- src/Components/Row.js
+++ src/Components/Row.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import "./../styles/Row.css";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
-function Row({ title, fetchUrl }) {
+function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
 
   //pulling information from the tmdb using its API
@@ -25,10 +26,12 @@ function Row({ title, fetchUrl }) {
       <div className="row__posters">
         {movies.map((movie) => (
           <img
-            src={`${base_url}` + movie.poster_path}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
             alt={movie.name}
             key={movie.id}
-            className="row__poster"
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
           />
         ))}
       </div>
